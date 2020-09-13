a70a1d39d080a29baeabc00159734c6d7d1f6469
Ted <teddsbennett@gmail.com>
2020-09-13 22:21:57 +0200

    Add arg parsing, rename vars

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 const { data } = require('./data');
 
 process.stdin.setRawMode(true);
-
 process.stdin.resume();
-
 process.stdin.setEncoding('utf8');
 
-let keys = '';
+console.log([].join('') === '');
+
+let query = process.argv.length > 2 ? process.argv.slice(2).join('') : '';
 let results = [];
 console.clear();
-console.log('Enter search: ');
+console.log(`Enter search: ${query}`);
 
 process.stdin.on('data', (key) => {
   // Exit: Ctrl-C, or type 'q' or 'exit' and hit enter
-  if (key === '\u0003' || ((keys === 'exit' || keys === 'q') && key === '\u000d')) {
+  if (key === '\u0003' || ((query === 'exit' || query === 'q') && key === '\u000d')) {
     process.exit();
   } else if (key === '\u007f') {
     // Delete
-    keys = keys.slice(0, -1);
+    query = query.slice(0, -1);
   } else if (key === '\u0015') {
     // Command-Delete - delete entire search field
-    keys = '';
+    query = '';
   } else if (key === '\u001B\u007f') {
     // Option-Delete - delete last word
     const re = /\b(\w+)(\s*)$/;
-    const index = keys.search(re);
-    keys = keys.slice(0, index);
+    const index = query.search(re);
+    query = query.slice(0, index);
   } else {
     // Add keypress to search field
-    keys = (keys + key).replace(/\r?\n|\r/g, '');
+    query = (query + key).replace(/\r?\n|\r/g, '');
   }
 
   console.clear();
-  console.log(`Enter search: ${keys}`);
+  console.log(`Enter search: ${query}`);
 
-  if (keys !== '') {
+  if (query !== '') {
     results = Object.keys(data)
-      .filter((command) => command.includes(keys))
+      .filter((command) => command.includes(query))
       .slice(0, process.stdout.rows - 2);
 
     results.forEach((result) => console.log(`${data[result].padEnd(10, ' ')} - ${result}`));
